5d4daa8a311e3e7de9a1077f42784c2aca316b43
mills32 <biosystemxy@gmail.com>
2022-11-03 22:46:35 +0100

    update

--- make.bat
+++ make.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 rem required Turbo C installed at C:\TC
 rem compile c files
 tcc -G -Ic:\tc\include -mc -c player.c plugins.c 
@@ -11,17 +10,3 @@ rem link
 tlink c0c.obj player.obj ad_lib.lib,adplay88.exe,, cc -Lc:\tc\lib
 del *.OBJ
 del *.MAP
-=======
-rem required Turbo C installed at C:\TC
-rem compile c files
-tcc -G -O2 -Ic:\tc\include -mc -c player.c plugins.c 
-rem compile assembly files
-rem tasm /dc lt_sprc.asm
-rem create lib
-del *.LIB
-tlib ad_lib.lib +plugins.obj
-rem link
-tlink c0c.obj player.obj ad_lib.lib,adplay88.exe,, cc -Lc:\tc\lib
-del *.OBJ
-del *.MAP
->>>>>>> 7f575023bf2dcb27edf3351269ef7b7b78484e2a
